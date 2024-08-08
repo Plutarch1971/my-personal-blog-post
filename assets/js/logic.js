e96b48cc6e396d553eea07ad2d3de616b8401f64
Matt-Verni <matthewpmendez@gmail.com>
2024-08-08 10:34:35 -0400

    Updated All Files

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,10 @@
 // TODO: Create logic to toggle the light/dark mode styles for the page and circle. The mode should be saved to local storage.
 const toggleSwitch = document.getElementById('toggle');
-const dataTheme = document.documentElement.getAttribute('data-theme');    
+//const dataTheme = document.documentElement.getAttribute('data-theme');    
 
 var currentTheme = localStorage.getItem('theme') || 'light';
-console.log(dataTheme);
-console.log(currentTheme);
+//console.log(dataTheme);
+//console.log(currentTheme);
 const switchTheme = function(){
     const body = document.body;
 if (body.classList.contains("dark")){
